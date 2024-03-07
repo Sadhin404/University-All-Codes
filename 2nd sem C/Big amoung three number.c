6dba03addde1ccd93232efb073fb0c83e69e650f
Sadhin khan <sadhin.pro404@gmail.com>
2024-03-08 05:53:19 +0600

    Create Big amoung three number.c

---
+++ 2nd sem C/Big amoung three number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if (a > b) {
+        if (a > c) {
+            printf("A is big");
+        } else {
+            printf("C is big");
+        }
+    } else {
+        if (b > c) {
+            printf("B is big");
+        } else {
+            printf("C is big");
+        }
+    }
+    
+    return 0;
+}
