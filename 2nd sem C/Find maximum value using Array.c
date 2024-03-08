1e69e710d4653c3def5019af260e01e42f7a97da
Sadhin khan <sadhin.pro404@gmail.com>
2024-03-08 16:05:48 +0600

    Create Find maximum value using Array.c

---
+++ 2nd sem C/Find maximum value using Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; ++i) {
+        if (arr[0] < arr[i]) {
+            arr[0] = arr[i];
+        }
+    }
+
+    printf("%d", arr[0]);
+
+    return 0;
+}
